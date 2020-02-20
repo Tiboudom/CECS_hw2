18e3f0562341560a92d579a85a4cf8915587fd71
Olivier <olivier.de-la-sayette@epitech.eu>
2020-02-20 02:30:38 +0100

    basic main parsing

--- src/client/src/main.c
+++ src/client/src/main.c
@@ -5,11 +5,49 @@
 ** main.c
 */
 
+#include <stdio.h>
+#include <stdlib.h>
 #include "client.h"
+
+static	void	connect_to_server(int ip, int port)
+{
+	printf("Connected!\n");
+	printf("IP is : %d\n", ip);
+	printf("Port is : %d\n", port);
+}
+
+static	int	get_ip(int ac, char **av)
+{
+	if (ac != 3) {
+		printf("USAGE : ./client \"IP Address\" \"port number\"\n");
+		exit(84);
+	} else {
+		int ip = atoi(av[1]);
+		if (ip <= 0) {
+			printf("USAGE : ./client \"IP Address\" \"port number\"\n");
+			exit(84);
+		}
+		return (ip);
+	}
+}
+
+static	int	get_port(int ac, char **av)
+{
+	if (ac != 3) {
+		printf("USAGE : ./client \"IP Address\" \"port number\"\n");
+		exit(84);
+	} else {
+		int port = atoi(av[2]);
+		if (port <= 0) {
+			printf("USAGE : ./client \"IP Address\" \"port number\"\n");
+			exit(84);
+		}
+		return (port);
+	}
+}
 
 int	main(int ac, char **av)
 {
-	(void)ac;
-	(void)av;
+	connect_to_server(get_ip(ac, av), get_port(ac, av));
 	return (0);
 }
--- src/server/src/main.c
+++ src/server/src/main.c
@@ -5,11 +5,33 @@
 ** main.c
 */
 
+#include <stdio.h>
+#include <stdlib.h>
 #include "server.h"
+
+static	void	connect_port(int port)
+{
+	printf("Connected!\n");
+	printf("Port is : %d\n", port);
+}
+
+static	int	get_port(int ac, char **av)
+{
+	if (ac != 2) {
+		printf("USAGE : ./server \"port number\"\n");
+		exit(84);
+	} else {
+		int port = atoi(av[1]);
+		if (port <= 0) {
+			printf("USAGE : ./server \"port number\"\n");
+			exit(84);
+		}
+		return (port);
+	}
+}
 
 int	main(int ac, char **av)
 {
-	(void)ac;
-	(void)av;
+	connect_port(get_port(ac, av));
 	return (0);
 }
