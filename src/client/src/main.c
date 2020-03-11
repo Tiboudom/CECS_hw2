1f768caec50380fa468def7518ca16ced11ae3ba
Olivier <olivier.de-la-sayette@epitech.eu>
2020-03-11 07:19:22 +0100

    client

--- src/client/src/main.c
+++ src/client/src/main.c
@@ -7,47 +7,72 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include "client.h"
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <string.h>
 
-static	void	connect_to_server(int ip, int port)
+static char 			*get_input(int sock)
 {
-	printf("Connected!\n");
-	printf("IP is : %d\n", ip);
-	printf("Port is : %d\n", port);
+	ssize_t			fd;
+	size_t			len;
+	char			buf[2];
+	char 			*str;
+
+	str = malloc(sizeof(char) * 1);
+	str[0] = '\0';
+	while ((fd = read(sock, buf, 1)) > 0) {
+		buf[fd] = '\0';
+		str = strcat(realloc(str, strlen(str) + 2), buf);
+		len = strlen(str);
+		if (strlen(str) >= 2 &&
+			strcmp(str + (len - 2), "\r\n") == 0) {
+			str[len - 2] = '\0';
+			return (str);
+		} else if (strlen(str) >= 1 &&
+			strcmp(str + (len - 1), "\n") == 0) {
+			str[len - 1] = '\0';
+			return (str);
+		}
+	}
+	return (str);
 }
 
-static	int	get_ip(int ac, char **av)
+static void			connect_client(char *ip, int port)
 {
-	if (ac != 3) {
-		printf("USAGE : ./client \"IP Address\" \"port number\"\n");
-		exit(84);
-	} else {
-		int ip = atoi(av[1]);
-		if (ip <= 0) {
-			printf("USAGE : ./client \"IP Address\" \"port number\"\n");
-			exit(84);
-		}
-		return (ip);
-	}
-}
+	int			sock = 0;
+	struct sockaddr_in	serv_addr;
+	char 			*str;
 
-static	int	get_port(int ac, char **av)
-{
-	if (ac != 3) {
-		printf("USAGE : ./client \"IP Address\" \"port number\"\n");
-		exit(84);
-	} else {
-		int port = atoi(av[2]);
-		if (port <= 0) {
-			printf("USAGE : ./client \"IP Address\" \"port number\"\n");
-			exit(84);
-		}
-		return (port);
+	if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+		printf("Socket creation error\n");
+		exit(-1);
 	}
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_port = htons(port);
+
+	if(inet_pton(AF_INET, ip, &serv_addr.sin_addr) <= 0) {
+		printf("Invalid address\n");
+		exit(-1);
+	}
+	if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+		printf("Connection Failed\n");
+		exit(-1);
+	}
+	printf("Connection established\n");
+	printf("IP: %s Port: %d\n", ip, port);
+	str = get_input(sock);
+	printf("%s\n", str);
+	free(str);
 }
 
 int	main(int ac, char **av)
 {
-	connect_to_server(get_ip(ac, av), get_port(ac, av));
-	return (0);
+	if (ac != 3) {
+		printf("USAGE : ./client \"IP Address\" \"port number\"\n");
+		exit(-1);
+	} else {
+		connect_client(av[1], atoi(av[2]));
+	}
+	return 0;
 }
